3dd3790f6ef3b4084824b4b063263aaafc794c01
maornezer <maorne@post.bgu.ac.il>
2024-06-11 13:21:56 +0300

    rd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Princes world!");
+        System.out.println("Hello Princes world new !");
     }
 }
