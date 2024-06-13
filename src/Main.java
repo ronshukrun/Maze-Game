b6373bfe0d6528278276ae2db85d32d15b8b38f8
ronshukrun <ronshu@post.bgu.ac.il>
2024-06-13 17:48:17 +0300

    ijujk

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Princes world new !");
+        System.out.println("Hello Princes kkkworld new !");
     }
     ///
 }
