f096ad9ae39aaaeccdda7e189ee247a7cdca5ade
maornezer <maorne@post.bgu.ac.il>
2024-07-07 16:19:03 +0300

    Client

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,4 +1,30 @@
 package Client;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Client {
+    private InetAddress serverIP;
+    private int serverPort;
+    private IClientStrategy clientStrategy;
+
+    public Client(InetAddress serverIP, int serverPort, IClientStrategy clientStrategy) {
+        if (serverIP == null || clientStrategy == null || serverPort <= 0) {
+            throw new IllegalArgumentException("Invalid parameters for Client constructor.");
+        }
+        this.serverIP = serverIP;
+        this.serverPort = serverPort;
+        this.clientStrategy = clientStrategy;
+    }
+
+    public void communicateWithServer() {
+        try (Socket serverSocket = new Socket(serverIP, serverPort)) {
+            //System.out.println("Connected to server - IP = " + serverIP + ", Port = " + serverPort);
+            clientStrategy.clientStrategy(serverSocket.getInputStream(), serverSocket.getOutputStream());
+        } catch (IOException e) {
+            System.err.println("Error communicating with server: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
